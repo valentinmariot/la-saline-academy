cee106ecca94115b74636c0e3da2db2537f3d483
valentin Mariot <valentinmariot@gmail.com>
2023-07-07 13:56:55 +0200

    [FIX]: icon sizes

--- components/basicIcon/basicIcon.tsx
+++ components/basicIcon/basicIcon.tsx
@@ -2,10 +2,11 @@ import { FC } from "react";
 
 interface BasicIconProps {
   name: string;
+  size?: "s" | "m" | "l" | "xl";
 }
 
-const BasicIcon: FC<BasicIconProps> = ({ name }) => {
-  const classes = `ico ico-${name}`;
+const BasicIcon: FC<BasicIconProps> = ({ name, size = "m" }) => {
+  const classes = `ico ico-${name} ico-${size}`;
   return <span className={classes} />;
 };
 
