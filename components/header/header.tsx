673974a2a730d955e433da20e657ae7ca82998b9
Thomas JEU <jeuthomas03@gmail.com>
2023-07-07 10:42:14 +0200

    [fix] toggle menu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,25 +1,33 @@
+// header.tsx
+import React from "react";
 import styles from "./header.module.scss";
 import BasicIcon from "../basicIcon/basicIcon";
 
-const Header = () => {
+interface HeaderProps {
+  onBurgerMenuClick: () => void;
+}
+
+const Header = ({ onBurgerMenuClick }: HeaderProps) => {
   return (
-    <>
-      <header className={styles.header + " glassmorphism"}>
-        <div>
-          <BasicIcon name="burger-menu" />
-          <a href="#">
-            <img src="#" alt="Logo Saline" />
+      <>
+        <header className={styles.header + " glassmorphism"}>
+          <div>
+            <button className="white" onClick={onBurgerMenuClick}>
+              <BasicIcon name="burger-menu"/>
+            </button>
+            <a href="#">
+              <img src="#" alt="Logo Saline" />
+            </a>
+          </div>
+
+          <div></div>
+
+          <a href="" className="btn btn-purple-solid-intense">
+            <BasicIcon name="profile" />
+            Mon profil
           </a>
-        </div>
-
-        <div></div>
-
-        <a href="" className="btn btn-purple-solid-intense">
-          <BasicIcon name="profile" />
-          Mon profil
-        </a>
-      </header>
-    </>
+        </header>
+      </>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,50 @@
+// App.tsx
+import React, { useState } from "react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { motion, AnimatePresence } from "framer-motion";
 import "../styles/global.scss";
 import Header from "@/components/header/header";
 import MenuLateral from "@/components/menuLateral/menuLateral";
+import styles from "@/components/menuLateral/menuLateral.module.scss";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <AnimatePresence>
-        <motion.div
-          // key={router.route}
-          exit="pageExit"
-          initial="pageInitial"
-          animate="pageAnimate"
-          variants={{
-            pageInitial: {
-              opacity: 0,
-            },
-            pageAnimate: {
-              opacity: 1,
-            },
-            pageExit: {
-              opacity: 0,
-            },
-          }}
-        >
-          <Header />
-          <main>
-            <MenuLateral />
-            <div className="container">
-              <div className="content border">
-                <Component {...pageProps} />
-              </div>
-            </div>
-          </main>
-        </motion.div>
-      </AnimatePresence>
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    const [isMenuMoved, setMenuMoved] = useState(false);
+
+    const handleBurgerMenuClick = () => {
+        setMenuMoved(!isMenuMoved);
+    };
+
+    return (
+        <SessionProvider session={session}>
+            <AnimatePresence>
+                <motion.div
+                    exit="pageExit"
+                    initial="pageInitial"
+                    animate="pageAnimate"
+                    variants={{
+                        pageInitial: {
+                            opacity: 0,
+                        },
+                        pageAnimate: {
+                            opacity: 1,
+                        },
+                        pageExit: {
+                            opacity: 0,
+                        },
+                    }}
+                >
+                    <Header onBurgerMenuClick={handleBurgerMenuClick} />
+                    <main>
+                        <MenuLateral isMenuMoved={isMenuMoved} />
+                        <div className={"container " + `${isMenuMoved ? styles.menuMoved : "smallContainer"}`}>
+                            <div className="content border">
+                                <Component {...pageProps} />
+                            </div>
+                        </div>
+                    </main>
+                </motion.div>
+            </AnimatePresence>
+        </SessionProvider>
+    );
 }
