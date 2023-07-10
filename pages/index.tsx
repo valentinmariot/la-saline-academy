0469d293a8a2e507f1cecfd2b7a119af5ecf54d7
Maxime Palmino <maximepalmino.pro@gmail.com>
2023-07-10 15:33:07 +0200

    [ADD]Rending session home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,41 @@ import Header from "@/components/header/header";
 import MenuLateral from "@/components/menuLateral/menuLateral";
 import styles from "@/components/menuLateral/menuLateral.module.scss";
 import React, { useState } from "react";
+import { useSession } from "next-auth/react";
 
 const Home = () => {
+  const session = useSession();
   const [isMenuMoved, setMenuMoved] = useState(false);
 
   const handleBurgerMenuClick = () => {
     setMenuMoved(!isMenuMoved);
   };
-  return (
-    <>
-      <Head>
-        <title>Accueil - La Saline Academy</title>
-      </Head>
-      <Header onBurgerMenuClick={handleBurgerMenuClick} />
-      <main>
-        <MenuLateral isMenuMoved={isMenuMoved} />
-        <div
-          className={
-            "container " +
-            `${isMenuMoved ? styles.menuMoved : "smallContainer"}`
-          }
-        >
-          <div className="content border">{/*// content*/} hey</div>
-        </div>
-      </main>
-    </>
-  );
+
+  if (session.status === "authenticated") {
+    return (
+      <>
+        <Head>
+          <title>Accueil - La Saline Academy</title>
+        </Head>
+        <Header onBurgerMenuClick={handleBurgerMenuClick} />
+        <main>
+          <MenuLateral isMenuMoved={isMenuMoved} />
+          <div
+            className={
+              "container " +
+              `${isMenuMoved ? styles.menuMoved : "smallContainer"}`
+            }
+          >
+            <div className="content border">{/*// content*/} hey</div>
+          </div>
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <h1>Home page</h1>
+    );
+  }
 };
 
 export default Home;
