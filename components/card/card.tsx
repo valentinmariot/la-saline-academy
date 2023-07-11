eb6ed1de8858bbed336f492ce0fb3f0c63713567
Thomas JEU <jeuthomas03@gmail.com>
2023-07-11 14:38:24 +0200

    [ADD] decoupage page profil responsiv && composant card_cours && composant card_note

---
+++ components/card/card.tsx
@@ -0,0 +1,79 @@
+import { FC } from "react";
+import Tag from "../tag/tag";
+import Link from "next/link";
+
+import styles from "./card.module.scss";
+import BasicIcon from "@/components/basicIcon/basicIcon";
+
+interface CardProps {
+    image: string;
+    title: string;
+    href: string;
+    author: string;
+    desc: string;
+    tagLink: string;
+    time?: string;
+    language?: string;
+    point?: string;
+    isNew?: boolean;
+    tagName?: string;
+    tagColor?: "purple" | "orange";
+}
+
+const Card: FC<CardProps> = ({
+    image,
+    title,
+    href= "#",
+    author,
+    time,
+    language,
+    point,
+    desc,
+    isNew= true,
+    tagName,
+    tagColor = "purple",
+    tagLink,
+}) => {
+    return (
+        <Link className={styles.card_cours + ' border'} href={href}>
+            {isNew &&
+                <span className={styles.new}>
+                     <Tag name="Nouveaux" href="#" size="xs" color="new"/>
+                </span>
+            }
+            {image &&
+                <figure>
+                    <img src={image} alt={title}/>
+                </figure>
+            }
+            <div className={styles.card_content}>
+                <div className={styles.top}>
+                    {title && <h5 className={styles.title}>{title}</h5>}
+                    {author && <p>Par <span className="purple">{author}</span></p>}
+                    <div className={styles.infos}>
+                        {time &&
+                            <p>{time}h</p>
+                        }
+                        {language &&
+                            <div>
+                                <BasicIcon name="chat-solo" size="s"/>
+                                {language}
+                            </div>
+                        }
+                        {point &&
+                            <div>
+                                <BasicIcon name="poin" size="s"/>
+                                {point}pts
+                            </div>
+                        }
+                    </div>
+                </div>
+                {desc && <p className={styles.desc}>{desc}</p>}
+                {tagName && <Tag name={tagName} href={tagLink} size="xs" color={tagColor}/>}
+            </div>
+
+        </Link>
+    );
+};
+
+export default Card;
--- pages/profil/index.tsx
+++ pages/profil/index.tsx
@@ -1,8 +1,75 @@
 import Head from "next/head";
 import BasicIcon from "@/components/basicIcon/basicIcon";
 import styles from "./profil.module.scss";
+import Tag from "@/components/tag/tag";
+import Card from "@/components/card/card";
+import Link from "next/link";
+import CardNote from "@/components/cardNote/cardNote";
+import {useState} from "react";
 
 const Profil = () => {
+    const [showAllCardsNote, setShowAllCardsNote] = useState(false);
+
+    //TODO : fetch Notes from data
+    const cardsNote = [
+
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+        {
+            title: "Prendre en main le solfège",
+            desc: "Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège Ma superbe note liée à la leçon sur l’introduction au solfège ",
+            href: "cours/detail"
+        },
+    ];
+
+    const visibleCardsNote = showAllCardsNote ? cardsNote : cardsNote.slice(0, 4);
+    const handleToggleCardsNote = () => {
+        setShowAllCardsNote(!showAllCardsNote);
+    };
+
     return (
         <div className={styles.page_profil}>
             <Head>
@@ -13,10 +80,10 @@ const Profil = () => {
             <div className={styles.infos_profil + ' dflexcolumn'}>
                 <div className={styles.titre + ' dflex w100'}>
                     <h3>Charles-Lucas Maxime</h3>
-                    <a href="profil/edit" className="btn btn-purple-link hover-effect">
+                    <Link href="profil/edit" className="btn btn-purple-link hover-effect">
                         Modifier profil
                         <BasicIcon name="edit" size="s"/>
-                    </a>
+                    </Link>
                 </div>
                 <div className={styles.infos}>
                     <div id="nb_cours">
@@ -37,18 +104,74 @@ const Profil = () => {
                 </div>
                 <div id="tag" className={styles.tags}>
 
+                    <Tag name="violon" href=""/>
+                    <Tag name="guitare" href=""/>
+                    <Tag name="zazz" href="" color="orange"/>
+
                 </div>
             </div>
             <hr/>
             <div>
-                <h4>Reprendre vos <span className="purple">cours</span></h4>
-                <p>fefa</p>
+                <div className={styles.head_cours + ' dflex'}>
+                    <h4>Reprendre vos <span className="purple">cours</span></h4>
+                    <a href="" className="btn btn-gray-link hover-effect">Voir tout</a>
+                </div>
+                <div className="grid4">
+                    <Card author="Charles-Lucas Maxime"
+                          time="30"
+                          point="230"
+                          language="Français"
+                          desc="Découvrez l'art de lire et comprendre la musique avec notre cours de solfège en six leçons. Du mystère des notes et des clés à l'interprétation des rythmes, des silences et des altérations, nous vous guidons à travers le labyrinthe du langage musical. Que vous soyez un musicien en herbe ou un virtuose en devenir, notre cours est conçu pour vous aider à déchiffrer les partitions et à enrichir votre expérience musicale. Rejoignez-nous et transformez les symboles et les signes sur le papier en mélodies et harmonies expressives."
+                          href="cours/detail"
+                          image="https://www.cenon.fr/sites/default/files/styles/actu_evenement_1400x805px/public/2019-04/4-ecole-de-musique-concert-enfants-cenon%40CGoussard.jpg?h=2a02e5e1&itok=24jRwHjg"
+                          tagLink="#"
+                          tagName="Violon"
+                          title="Prendre en main le solfège"/>
+                    <Card author="Charles-Lucas Maxime"
+                          time="30"
+                          point="230"
+                          language="Français"
+                          desc="Découvrez l'art de lire et comprendre la musique avec notre cours de solfège en six leçons. Du mystère des notes et des clés à l'interprétation des rythmes, des silences et des altérations, nous vous guidons à travers le labyrinthe du langage musical. Que vous soyez un musicien en herbe ou un virtuose en devenir, notre cours est conçu pour vous aider à déchiffrer les partitions et à enrichir votre expérience musicale. Rejoignez-nous et transformez les symboles et les signes sur le papier en mélodies et harmonies expressives."
+                          href="cours/detail"
+                          image="https://www.cenon.fr/sites/default/files/styles/actu_evenement_1400x805px/public/2019-04/4-ecole-de-musique-concert-enfants-cenon%40CGoussard.jpg?h=2a02e5e1&itok=24jRwHjg"
+                          tagLink="#"
+                          tagName="Violon"
+                          title="Prendre en main le solfège"/>
+                    <Card author="Charles-Lucas Maxime"
+                          time="30"
+                          point="230"
+                          language="Français"
+                          desc="Découvrez l'art de lire et comprendre la musique avec notre cours de solfège en six leçons. Du mystère des notes et des clés à l'interprétation des rythmes, des silences et des altérations, nous vous guidons à travers le labyrinthe du langage musical. Que vous soyez un musicien en herbe ou un virtuose en devenir, notre cours est conçu pour vous aider à déchiffrer les partitions et à enrichir votre expérience musicale. Rejoignez-nous et transformez les symboles et les signes sur le papier en mélodies et harmonies expressives."
+                          href="cours/detail"
+                          image="https://www.cenon.fr/sites/default/files/styles/actu_evenement_1400x805px/public/2019-04/4-ecole-de-musique-concert-enfants-cenon%40CGoussard.jpg?h=2a02e5e1&itok=24jRwHjg"
+                          tagLink="#"
+                          tagName="Violon"
+                          title="Prendre en main le solfège"/>
+                    <Card author="Charles-Lucas Maxime"
+                          time="30"
+                          point="230"
+                          language="Français"
+                          desc="Découvrez l'art de lire et comprendre la musique avec notre cours de solfège en six leçons. Du mystère des notes et des clés à l'interprétation des rythmes, des silences et des altérations, nous vous guidons à travers le labyrinthe du langage musical. Que vous soyez un musicien en herbe ou un virtuose en devenir, notre cours est conçu pour vous aider à déchiffrer les partitions et à enrichir votre expérience musicale. Rejoignez-nous et transformez les symboles et les signes sur le papier en mélodies et harmonies expressives."
+                          href="cours/detail"
+                          image="https://www.cenon.fr/sites/default/files/styles/actu_evenement_1400x805px/public/2019-04/4-ecole-de-musique-concert-enfants-cenon%40CGoussard.jpg?h=2a02e5e1&itok=24jRwHjg"
+                          tagLink="#"
+                          tagName="Violon"
+                          title="Prendre en main le solfège"/>
+                </div>
             </div>
             <hr/>
             <div>
-
+                <h4><BasicIcon name="edit" size="m"/> Quick notes</h4>
+                <div className={styles.listing_notes + ' grid2'}>
+                    {visibleCardsNote.map((cardNote, index) => (
+                        <CardNote key={index} title={cardNote.title} desc={cardNote.desc} href={cardNote.href} />
+                    ))}
+                </div>
+                <button className="btn btn-purple-link hover-effect ml-auto" onClick={handleToggleCardsNote}>
+                    {showAllCardsNote ? 'Afficher moins' : 'Afficher plus'}
+                    <BasicIcon name={showAllCardsNote ? 'circle-arrow-up' : 'circle-arrow-down'} size="s" />
+                </button>
             </div>
-
         </div>
     );
 };
